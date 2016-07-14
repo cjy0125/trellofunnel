a7e6551a3bf90f6bf95225f64420bc422730ec76
Charles Yang <cjy0125@livemail.tw>
2016-07-14 14:06:14 +0800

    Update for pip package

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
 # setup
+from setuptools import setup
+
+setup(name='trellofunnel',
+      version='0.1',
+      description='Trello funnel is using for filter cards by Trello exported JSON',
+      url='https://cjy0125@github.com/cjy0125/trellofunnel',
+      author='Charles Yang',
+      author_email='cjy0125@livemail.tw',
+      license='MIT',
+      packages=['funniest'],
+      zip_safe=False)
