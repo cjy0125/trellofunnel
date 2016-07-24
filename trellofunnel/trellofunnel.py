6fce982ccdc5ec3a105a0ab67b4763dc83d5db6d
Charles Yang <cjy0125@livemail.tw>
2016-07-24 17:56:43 +0800

    Add docstring and apply lint by prospector

--- trellofunnel/trellofunnel.py
+++ trellofunnel/trellofunnel.py
@@ -2,12 +2,13 @@
 import json
 import time
 import datetime
-import types
 import requests
 
 class trellofunnel():
+    """This is a simple python class to filter Trello cards by keys,
+and the data source is an exported JSON from Trello.\n"""
     def __init__(self, **kwargs):
-        if kwargs.has_key('file'):
+        if 'file' in kwargs:
             self.loadFromFile(kwargs.get('file'))
         else :
             self.loadFromTrello(kwargs)
@@ -18,28 +19,31 @@ class trellofunnel():
         self.cards = self.rawdata.pop('cards', None)
         self.labels = self.rawdata.pop('labels', None)
         self.lists = self.rawdata.pop('lists', None)
-        self.normalizeDateLastActivity()
+        self.__normalizeDateLastActivity()
         self.updateListsMapping()
         self.updateLabelsMapping()
 
+    def __normalizeDateLastActivity(self):
+        for i in range(len(self.cards)):
+            self.cards[i]['dateLastActivity'] = self.__timeToTimstamp(self.cards[i]['dateLastActivity'])
+
+    def __timeToTimstamp(self, timestr):
+        return time.mktime(datetime.datetime.strptime(timestr[:19], "%Y-%m-%dT%H:%M:%S").timetuple())
 
     def loadFromFile(self, file):
+        """Load JSON from file\nArgs:\n\tfile (str) : file path"""
         with open(file) as f:
             self.__parseCards(f.read())
 
     def loadFromTrello(self, kwargs):
+        """Load JSON from Trello online (developing)"""
         cookies = dict(item.split('=', 1) for item in kwargs.get('cookies').split(';'))
         r = requests.get(kwargs.get('url'), cookies = cookies)
         self.__parseCards(r.text)
 
-    def normalizeDateLastActivity(self):
-        for i in range(len(self.cards)):
-            self.cards[i]['dateLastActivity'] = self.timeToTimstamp(self.cards[i]['dateLastActivity'])
-
-    def timeToTimstamp(self, timestr):
-        return time.mktime(datetime.datetime.strptime(timestr[:19], "%Y-%m-%dT%H:%M:%S").timetuple())
-
     def viewCardsByKeys(self, keys):
+        """Get the view of remaining cards\nArgs:\n\tkeys array(str) : fields what you want to show
+Return:\n\tstr : a string for each fields of remaining cards"""
         result = ''
         for i in range(len(self.cards)):
             output = ''
@@ -56,24 +60,29 @@ class trellofunnel():
         return result
 
     def cardsFilterByLabels(self, filter):
+        """TBD"""
         pass
 
     def cardsFilterByMembers(self, filter):
+        """TBD"""
         pass
 
     def cardsFilterByList(self, listId):
+        """Filter cards by ID of list\nArgs:\n\tlistId str : refer self.mapLists to get listId\n"""
         for i in reversed(range(len(self.cards))):
             if self.cards[i]['idList'] != listId:
                 self.cards.pop(i)
 
     def cardsFilterByClosed(self, boolean = False):
+        """Filter cards by closed flag, default would filter out closed cards.
+Args:\n\tboolean (bool) : Flag only available for True/False\n"""
         for i in reversed(range(len(self.cards))):
             if self.cards[i]['closed'] != boolean:
                 self.cards.pop(i)
 
     def cardsFilterByLastUpdateDate(self, days):
-        # 86400 = 60*60*24 sec
-        threshold = time.time() - days * 86400
+        """Filter cards by activate in recent n days\nArgs:\n\tdays (int) : number of days\n"""
+        threshold = time.time() - days * 86400  # 86400 = 60*60*24 sec
         for i in reversed(range(len(self.cards))):
             if self.cards[i]['dateLastActivity'] < threshold:
                 self.cards.pop(i)
@@ -88,4 +97,5 @@ class trellofunnel():
         for i in self.labels:
             self.mapLabels[i['id']] = i['name']
 
-
+if __name__ == '__main__':
+    pass
